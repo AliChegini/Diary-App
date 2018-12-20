1772d499f3eb844c641697d943bea20da65fc97d
Ali Chegini <ali.c.engineer@gmail.com>
2018-12-20 18:41:11 +0100

    added Unit Test target

---
+++ DiaryAppTests/DiaryAppTests.swift
@@ -0,0 +1,18 @@
+//
+//  DiaryAppTests.swift
+//  DiaryAppTests
+//
+//  Created by Ehsan on 20/12/2018.
+//  Copyright © 2018 Ali C. All rights reserved.
+//
+
+import XCTest
+@testable import DiaryApp
+
+class DiaryAppTests: XCTestCase {
+
+    func testDemoUnit() {
+        
+    }
+
+}
